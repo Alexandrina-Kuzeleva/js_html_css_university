47757cf536943610a5f436f2aa291a815fcf4ebb
Alexandrina <alaz2005tv@gmail.com>
2024-02-24 14:45:28 +0300

    task2

---
+++ Mondrian/sketch.js
@@ -0,0 +1,80 @@
+function setup()
+{
+	//create a large square canvas
+	createCanvas(800, 800);
+}
+
+function draw()
+{
+
+	//red
+	fill(255, 0, 0);
+
+	//black lines
+	strokeWeight(12);
+
+	//red rectangle, (x,y,size1,size2)
+	rect(100, 50, 600, 600);
+
+	//blue
+	fill(0, 0, 255);
+
+	//blue rectangle 
+	rect(100, 200, 100, 250);
+
+	//white
+	fill(255, 255, 255);
+	
+	//white rectangle 
+	rect(100, 50, 300, 150);
+
+	//white
+	fill(255, 255, 255);
+	
+	//white rectangle 
+	rect(100, 50, 125, 150);
+
+	//yellow
+	fill(255, 255, 0);
+	
+	//yellow rectangle 
+	rect(400, 50, 300, 400);
+
+	//yellow
+	fill(255, 255, 0);
+	
+	//yellow rectangle 
+	rect(400, 50, 300, 350);
+
+	//yellow
+	fill(255, 255, 0);
+	
+	//yellow rectangle 
+	rect(400, 50, 300, 235);
+
+	//yellow
+	fill(255, 255, 0);
+	
+	//yellow rectangle 
+	rect(400, 50, 200, 400);
+
+
+	//black
+	fill(0, 0, 0);
+	
+	//black rectangle 
+	rect(100, 450, 300, 200);
+
+	//grey
+	fill(128,128,128);
+	
+	//grey rectangle 
+	rect(400, 450, 300, 200);
+
+	//grey
+	fill(128,128,128);
+	
+	//grey rectangle 
+	rect(400, 450, 125, 200);
+
+}

5918c323ff12b62e06773424f12744bfc6f86c75
Alexandrina <alaz2005tv@gmail.com>
2024-03-02 15:53:55 +0300

    It did on class, 2nd of March

---
+++ testClass/sketch.js
@@ -0,0 +1,18 @@
+function setup()
+{
+	//create a large square canvas
+	createCanvas(400, 400);
+	fill(172,42,242)
+	stroke(212,94,35)
+}
+
+function draw()
+{
+	background(220)
+	line(23,23,400,300)
+	ellipse(100,20,100,20)
+	strokeWeight(10)
+	rect(30,100,30,200)
+	triangle(12,350,50,50,350,350)
+
+}
